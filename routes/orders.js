a004b322b93f602349a7d0bb3b5dd89c8426d876
Dylanx3x <www.dylanedwards74@gmail.com>
2026-05-27 21:50:36 +0600

    fix email ipv4

--- routes/orders.js
+++ routes/orders.js
@@ -7,12 +7,16 @@ const nodemailer = require('nodemailer');
 //  📧 EMAIL SETUP
 // ===================================
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  family: 4,
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 async function sendOrderEmail(order) {
